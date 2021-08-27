d38e50d72e578c82b99934dd959d9ef97ff4b59f
CRAKAAW <crowshiding@gmail.com>
2021-08-27 04:20:47 -0700

    Created a boss test

--- Assets/Scripts/NPC/MoveList/Shoot.cs
+++ Assets/Scripts/NPC/MoveList/Shoot.cs
@@ -8,15 +8,19 @@ namespace NPC
     {
         private Coroutine coroutine;
         private MoveLibrary.Shoot shoot;
+        private Vector3 direction;
 
         public Shoot(Controller controller) : base(controller)
         {
             shoot = (MoveLibrary.Shoot)Controller.CurrentMove;
+            direction = GameManager.Instance.player.transform.position - Controller.transform.position;
+            direction = direction.normalized;
         }
 
         public override void EnterState()
         {
-            GameObject.Instantiate(shoot.bullet, Controller.transform.position, Quaternion.identity);
+            GameObject bullet = Controller.Instantiate(shoot.bullet, Controller.transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shoot.speed * direction;
             Duration(Controller.CurrentMove.duration, ref coroutine);
         }
     }
